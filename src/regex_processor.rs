e3af0f78c5ed41ff1393d9366822a90f95361fe1
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2024-03-25 09:39:19 -0300

    agrego procesamiento de expresiones

--- src/regex_processor.rs
+++ src/regex_processor.rs
@@ -1,8 +1,17 @@
-use crate::{custom_error::CustomError, expresion::{Expresion, ExpresionNormal}};
+use crate::{
+    custom_error::CustomError, 
+    expresion::{self, Expresion, ExpresionNormal}
+};
 
-const PERIOD: char = '.';
+//const PERIOD: char = '.';
 const CARET: char = '^';
 const ASTERISK: char = '*';
+const PLUS: char = '+';
+const QUESTION_SIGN: char = '?';
+const OPEN_CURVY_BRACKET: char = '{';
+const CLOSE_CURVY_BRACKET: char = '}';
+
+
 
 pub fn validar_metacaracter(regular_expression: &str, caracter: char) -> bool {
     if let Some(indice) = regular_expression.chars().position(|c| c == caracter) {
@@ -34,63 +43,7 @@ impl RegexProcessor {
     //metacaracteres. Si encuentro metacaracteres,
     //tengo que actualizar el filtro.
     pub fn build(regular_expression: &str) -> Result<RegexProcessor, CustomError> {
-        
-        //Este vector contendra una separacion de la regex en pequenias
-        //expresiones.
-        let mut expresiones: Vec<String> = vec!["".to_string()];
-        let mut index_expresiones: usize = 1;
-
-        let largo_regex: usize = regular_expression.len();
-
-        let mut i: usize = 0;
-        let caracteres_regex: Vec<char> = regular_expression.chars().collect();
-
-        while i < largo_regex {
-            if caracteres_regex[i] == ASTERISK {
-                //index_expresiones += 1;
-                if index_expresiones > 0 {
-                    let mut repetition = String::new();
-                    let caracter = caracteres_regex[i - 1];
-                   // expresiones[index_expresiones - 1].remove(expresiones[index_expresiones - 1].len() - 1);
-
-                    repetition.push(caracter);
-                    repetition.push(caracteres_regex[i]);
-
-                    expresiones.push(repetition);
-
-                    if index_expresiones != 1 {
-                        index_expresiones += 1;
-                    }
-                }
-            } else {
-                if index_expresiones == 1 {
-                    expresiones[index_expresiones - 1].push(caracteres_regex[i]);
-                } else {
-                    expresiones[index_expresiones].push(caracteres_regex[i]);
-                }
-            }
-            i += 1;
-        }
-
-        println!("{:?}", expresiones);
-        
-        
-
-
-       // for caracter in regular_expression.chars() {
-         //   match caracter {
-                //PERIOD => validar_metacaracter(regular_expression, caracter),
-                // CARET => validar_metacaracter(regular_expression, caracter),
-           //     ASTERISK => println!("Soy un asterisk!"),
-             //   _ => println!("Encontre un caracter normal")
-            //}
-        //}
-
-        //si mi vector de expresiones termina vacio
-        //quiere decir que no encontre ningun metacaracter :D
-       // if separacion_expresiones.len() == 0 {
-         //   separacion_expresiones.push(Box::new(ExpresionNormal::new(regular_expression)));
-        //}
+        RegexProcessor::procesar_expresion(regular_expression);
 
         Ok(RegexProcessor {
             regular_expression: regular_expression.to_string(), 
@@ -98,6 +51,54 @@ impl RegexProcessor {
         })
     }
 
+    fn procesar_expresion(regular_expression: &str) {
+        //Este vector contendra una separacion de la regex en pequenias
+        //expresiones.
+        let mut expresiones: Vec<String> = vec!["".to_string()];
+        let mut index_expresiones: usize = 0;
+
+        let caracteres_regex: Vec<char> = regular_expression.chars().collect();
+        let mut i: usize = 0;
+
+        while i < caracteres_regex.len() {
+            if caracteres_regex[i] == ASTERISK || caracteres_regex[i] == PLUS || caracteres_regex[i] == QUESTION_SIGN {
+                let mut expresion_anterior_modificada = expresiones[index_expresiones].to_string();
+                expresion_anterior_modificada = expresion_anterior_modificada[0..expresion_anterior_modificada.len() - 1].to_string();
+
+                expresiones[index_expresiones] = expresion_anterior_modificada;
+                
+                let mut nueva_expresion = String::new();
+                
+                nueva_expresion.push(caracteres_regex[i - 1]);
+                nueva_expresion.push(caracteres_regex[i]);
+                
+                expresiones.push(nueva_expresion);
+                expresiones.push("".to_string());
+                index_expresiones += 2;
+
+            } else if caracteres_regex[i] == OPEN_CURVY_BRACKET {
+                let mut contenido_dentro_del_bracket:String = String::new();
+
+                while caracteres_regex[i] != CLOSE_CURVY_BRACKET && i < caracteres_regex.len() - 1 {
+                    contenido_dentro_del_bracket.push(caracteres_regex[i]);
+                    i += 1;
+                }
+
+                contenido_dentro_del_bracket.push(caracteres_regex[i]);
+                
+                expresiones.push(contenido_dentro_del_bracket);
+                expresiones.push("".to_string());
+                index_expresiones += 2;
+                
+            } else {
+                expresiones[index_expresiones].push(caracteres_regex[i]);
+            }
+            i += 1;
+        }
+
+        println!("{:?}", expresiones);
+
+    }
    // pub fn filtrar_lecturas(&self, lineas: Vec<String>) -> Vec<String> {
      //   let mut resultado_filtro: Vec<String> = vec![];
                 
@@ -121,7 +122,7 @@ mod tests {
 
     #[test]
     fn test_01_filtro_dada_una_regex_compuesta_de_caracteres_normales() {
-        let regex_processor = RegexProcessor::build("abcd");
+        let regex_processor = RegexProcessor::build("a+hola*{abc}{holi}");
 
      //   assert_eq!(regex_processor.unwrap().filtrar_lecturas(vec!["abcdefg".to_string(), "fedcba".to_string()]), vec!["abcdefg".to_string()]);
     }
